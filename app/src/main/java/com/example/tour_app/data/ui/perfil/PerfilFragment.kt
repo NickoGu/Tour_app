2bfdc98acc81fdc302814a60a1bc59edb925eff0
NickoGu <92960428+NickoGu@users.noreply.github.com>
2023-11-22 03:04:14 -0300

    Vista de usuario básica y fuente de app nueva
    Creé una pantalla sencilla de usuario que muestra los datos dependiendo quien entra (luego se mejora) y cambié la fuente de la app

--- app/src/main/java/com/example/tour_app/data/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/example/tour_app/data/ui/perfil/PerfilFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.tour_app.databinding.FragmentPackagesBinding
+import repositories.PackageRepository
 import repositories.UserRepository
 
 class PerfilFragment : Fragment() {
 
     private var _binding: FragmentPackagesBinding? = null
     private val binding get() = _binding!!
+    private lateinit var tvFullName : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,9 +26,12 @@ class PerfilFragment : Fragment() {
         val userIdArg by navArgs<PerfilFragmentArgs>()
         println(userIdArg.userIdProfile)
         val user = UserRepository.getById(userIdArg.userIdProfile)
-
         _binding = FragmentPackagesBinding.inflate(inflater, container, false)
+        binding.tvFullName.text = "${user.name} ${user.surname} (${user.nickName})"
+        binding.tvMoney.text = "$" + user.money.toString()
         return binding.root
+
+
     }
 
 
